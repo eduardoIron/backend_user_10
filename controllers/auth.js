98eca3314999d3cc4aa7ef685febd73cd8053d8d
Eduardo <eduarwsuarez10@gmail.com>
2026-02-18 11:19:28 -0600

    usar num cuenta

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,31 @@
+const { Op } = require('sequelize');
 const { response } = require("express");
 const bcryptjs = require("bcryptjs");
 const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/generar-jwt");
 
 const login = async (req, res = response) => {
-    const { correo, pass } = req.body;
+    const { correo, pass, numCuenta } = req.body;
 
     try {
 
-        const usuario = await Usuario.findOne({ where: { correo } });
+        //const usuario = await Usuario.findOne({ where: { correo} });
+
+        const usuario = await Usuario.findOne({
+            where: {
+                [Op.or]: [
+                    { correo: correo || '' },     
+                    { numCuenta: numCuenta || '' }
+                ]
+            }
+        });
+
+        console.log(`NumCuenta: ${numCuenta}`);
+        console.log(`Correo: ${correo}`);
         
         if (!usuario) {
             return res.status(400).json({
-                msg: "Usuario no encontrado - verifique correo",
+                msg: "Usuario no encontrado - verifique Correo/Numero de cuenta",
             });
         }
 
@@ -40,7 +53,7 @@ const login = async (req, res = response) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({
-            msg: "Hable con el administrador",
+            msg: "Error al iniciar sesion intente mas tarde",
         });
     }
 };
